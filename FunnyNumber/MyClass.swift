bcdffdbb07cbe22cfcd0e9bb54a7d83d8199fa95
PKRU09 <pkru09@pkru09s-imac.local>
2017-07-27 16:38:28 +0700

    create class ok

---
+++ FunnyNumber/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  FunnyNumber
+//
+//  Created by PKRU09 on 7/27/2560 BE.
+//  Copyright © 2560 PKRU09. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    let strName = "Doramon"
+    
+    
+} //MyClass
--- FunnyNumber/ViewController.swift
+++ FunnyNumber/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+    
+    
+    @IBAction func DecreadButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func inCreadButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testClass = MyClass()
+        print("Name ==> \(testClass.strName)")
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
